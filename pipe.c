d5d868e2001c0ce8c541c95e0e41cdc10c7df7b1
zemlianin <detulie@mail.ru>
2023-04-18 00:09:46 +0300

    add-second-semaphore

--- pipe.c
+++ pipe.c
@@ -6,60 +6,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void child(int *pd, int semid)
+void child(int *pd, int semid, int semid2)
 {
-    char result[20] = "\0";
-    read(pd[0], result, 15);
-    printf("CHILD: %s\n", result);
-
-    struct sembuf child_buf =
-        {.sem_num = 0, .sem_op = 1, .sem_flg = 0};
-    if (semop(semid, &child_buf, 1) < 0)
+    for (size_t i = 0; i < 10; i++)
     {
-        printf("Can\'t add 1 to semaphor\n");
-        exit(-1);
+        /* code */
+
+        char result[20] = "\0";
+        read(pd[0], result, 15);
+        printf("CHILD: %s\n", result);
+        struct sembuf child_buf =
+            {.sem_num = 0, .sem_op = 1, .sem_flg = 0};
+
+        if (semop(semid, &child_buf, 1) < 0)
+        {
+            printf("Can\'t add 1 to semaphor\n");
+            exit(-1);
+        }
+        //        char *input;
+        //        scanf("%s", input);
+        write(pd[1], "inputChi", 15);
+        struct sembuf parent_buf =
+            {.sem_num = 0, .sem_op = -1, .sem_flg = 0};
+
+        if (semop(semid2, &parent_buf, 1) < 0)
+        {
+            printf("Can\'t sub 1 from semaphor\n");
+            exit(-1);
+        }
     }
-    write(pd[1], "Child to parent", 15);
 }
 
-void parent(int *pd, int semid)
+void parent(int *pd, int semid, int semid2)
 {
-    write(pd[1], "Parent to child", 15);
 
-    struct sembuf parent_buf =
-        {.sem_num = 0, .sem_op = -1, .sem_flg = 0};
-    if (semop(semid, &parent_buf, 1) < 0)
+    //    char *input;
+    //    scanf("%s", input);
+    for (size_t i = 0; i < 10; i++)
     {
-        printf("Can\'t sub 1 from semaphor\n");
-        exit(-1);
+        /* code */
+
+        write(pd[1], "inputPar", 15);
+        struct sembuf parent_buf =
+            {.sem_num = 0, .sem_op = -1, .sem_flg = 0};
+
+        if (semop(semid, &parent_buf, 1) < 0)
+        {
+            printf("Can\'t sub 1 from semaphor\n");
+            exit(-1);
+        }
+        char result[20] = "\0";
+        read(pd[0], result, 15);
+        printf("PARENT: %s\n", result);
+        struct sembuf child_buf =
+            {.sem_num = 0, .sem_op = 1, .sem_flg = 0};
+
+        if (semop(semid2, &child_buf, 1) < 0)
+        {
+            printf("Can\'t add 1 to semaphor\n");
+            exit(-1);
+        }
     }
-    char result[20] = "\0";
-    read(pd[0], result, 15);
-    printf("PARENT: %s\n", result);
 }
 
 int main()
 {
     int fd[2];
+
     if (pipe(fd) < 0)
     {
         printf("Pipe error\n");
         return -1;
     }
 
-    char pathname[] = "pipe-sem.c";
-    key_t key = ftok(pathname, 0);
+    char pathname[] = "pipes";
+    char pathname2[] = "pipes2";
+    key_t key = ftok(pathname, 14);
+    key_t key2 = ftok(pathname2, 15);
     int semid;
+    int semid2;
     struct sembuf mybuf;
 
     if ((semid = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL)) < 0)
     {
-        printf("Can\'t create semaphore\n");
+        printf("Can\'t create semaphore1\n");
         return -1;
     }
+
+    if ((semid2 = semget(key2, 1, 0666 | IPC_CREAT | IPC_EXCL)) < 0)
+    {
+        printf("Can\'t create semaphore2\n");
+        return -1;
+    }
+
     semctl(semid, 0, SETVAL, 0);
+    semctl(semid2, 0, SETVAL, 0);
 
     pid_t pid = fork();
+
     if (pid < 0)
     {
         printf("Fork error\n");
@@ -67,17 +111,24 @@ int main()
     }
     else if (pid == 0)
     {
-        child(fd, semid);
+        child(fd, semid, semid2);
     }
     else
     {
-        parent(fd, semid);
+        parent(fd, semid, semid2);
         wait(0);
         close(fd[0]);
         close(fd[1]);
+
         if (semctl(semid, 0, IPC_RMID, 0) < 0)
         {
-            printf("Can\'t delete semaphore\n");
+            printf("Can\'t delete semaphore1\n");
+            return -1;
+        }
+
+        if (semctl(semid2, 0, IPC_RMID, 0) < 0)
+        {
+            printf("Can\'t delete semaphore2\n");
             return -1;
         }
     }
